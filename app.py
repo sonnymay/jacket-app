ecca2708129290d5d222337c258846579a6f77eb
Sonny May <102835434+sonnymay@users.noreply.github.com>
2025-01-06 15:13:45 -0600

    Bind app to 0.0.0.0 and use PORT from environment

--- app.py
+++ app.py
@@ -462,4 +462,4 @@ scheduler.start()
 if __name__ == '__main__':
     if not os.path.exists(DATABASE):
         init_db()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
